be02e53aeeecb5fcebb901f3f5b507bb42d6244b
KSHcoding <133958664+KSHcoding@users.noreply.github.com>
2023-06-27 18:17:17 +0900

    Create 소스.cpp

---
+++ 프로그래머스/Level0/더 크게 합치기/소스.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+
+    string ab = to_string(a) + to_string(b);
+    string ba = to_string(b) + to_string(a);
+
+    answer = stoi(max(ab, ba));
+
+
+    return answer;
+}
